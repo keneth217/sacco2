09a5914089ee578b5499531425be42d77d8998b6
keneth217 <kipyegonkeneth03@gmail.com>
2023-06-29 13:22:39 +0300

    updates

--- src/main/java/com/sacco/Sacco/controller/MembersController.java
+++ src/main/java/com/sacco/Sacco/controller/MembersController.java
@@ -3,24 +3,28 @@ package com.sacco.Sacco.controller;
 import com.sacco.Sacco.models.Members;
 import com.sacco.Sacco.repository.MembersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/members")
 @CrossOrigin("*")
 public class MembersController {
-    private static final String UPLOAD_DIR = "/home/kennh/PROJECTS/sacco/src/images/";
+    private static final String UPLOAD_DIR = "/home/kennh/PROJECTS/Sacco/src/images/";
 
     @Autowired
     private MembersRepository membersRepository;
 
 
-    @PostMapping("/add")
+    @PostMapping(value = "/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public void addMember(
             @RequestParam("portImage") MultipartFile portImageFile,
             @RequestParam("formImage") MultipartFile formImageFile,
@@ -48,6 +52,9 @@ public class MembersController {
     ) throws IOException {
         if (portImageFile.isEmpty() || formImageFile.isEmpty()) {
             throw new IllegalArgumentException("Both portImage and formImage must be uploaded");
+        } long maxFileSize = 10 * 1024 * 1024; // 10MB in bytes
+        if (portImageFile.getSize() > maxFileSize || formImageFile.getSize() > maxFileSize) {
+            throw new IllegalArgumentException("File size exceeds the maximum limit of 10MB");
         }
 
         // Save portImage file
@@ -182,8 +189,65 @@ public Members searchMember(@RequestParam(value = "fullName", required = false) 
     } else {
         throw new IllegalArgumentException("Please provide either full name or ID number for the search.");
     }
+    }
+
+    @PostMapping("/import")
+    public void importMembers(@RequestParam("file") MultipartFile file) throws IOException {
+        if (file.isEmpty()) {
+            throw new IllegalArgumentException("Empty file uploaded.");
+        }
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            String line;
+            boolean isFirstLine = true;
+            List<Members> membersList = new ArrayList<>();
+
+            while ((line = reader.readLine()) != null) {
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    continue; // Skip the header line
+                }
+
+                String[] fields = line.trim().split(","); // Assuming each cell in the CSV is separated by a comma (,)
+
+                if (fields.length != 23) {
+                    throw new IllegalArgumentException("Invalid number of fields in the CSV file.");
+                }
+
+                // Create a new Members object
+                Members members = new Members();
+                members.setFullName(fields[0]);
+                members.setIdNo(fields[1]);
+                members.setKraPin(fields[2]);
+                members.setPhoneOne(fields[3]);
+                members.setPhoneTwo(fields[4]);
+                members.setEmail(fields[5]);
+                members.setUserName(fields[6]);
+                members.setOccupation(fields[7]);
+                members.setDob(fields[8]);
+                members.setCounty(fields[9]);
+                members.setConstituency(fields[10]);
+                members.setWard(fields[11]);
+                members.setAddress(fields[12]);
+                members.setGender(fields[13]);
+                members.setPosition(fields[14]);
+                members.setKinOne(fields[15]);
+                members.setKinOnePhone(fields[16]);
+                members.setRelationshipOne(fields[17]);
+                members.setKinTwo(fields[18]);
+                members.setKinTwoPhone(fields[19]);
+                members.setRelationshipTwo(fields[20]);
+                members.setPortImage(fields[21]);
+                members.setFormImage(fields[22]);
+
+                membersList.add(members);
+            }
+
+            membersRepository.saveAll(membersList);
+        }
+    }
 }
 
 
 
-}
+
